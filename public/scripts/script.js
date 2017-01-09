67dd1cc8d17d0fea3da2b4647ba97925feda020b
Amanda Jopp <aclaire488@gmail.com>
2017-01-09 16:43:26 -0600

    completed

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,46 @@
 console.log('sourced');
+
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('Hoth',['$scope', '$http', function($scope, $http){
+  console.log('totally angular dude');
+
+  $scope.addHero = function(){
+    console.log('button clicked');
+    var heroAdded = {
+      alias: $scope.aliasIn,
+      first_name: $scope.fNameIn,
+      last_name: $scope.lNameIn,
+      city: $scope.cityIn,
+      power_name: $scope.powerIn
+    };
+    console.log('hero added: ', heroAdded);
+    $http({
+      method:'POST',
+      url: '/hero',
+      data: heroAdded
+    }).then(function(response){
+      console.log('POST response: ', response);
+    });
+    clearForms();
+    $scope.find();
+  };//end addHero function
+
+  $scope.find = function(){
+   $http({
+     method: 'GET',
+     url: '/hero'
+   }).then(function(response){
+     console.log('GET response: ', response.data);
+     $scope.collection = response.data;
+   });
+ };//end find function//end find
+
+  var clearForms = function(){
+      $scope.aliasIn='';
+      $scope.fNameIn='';
+      $scope.lNameIn='';
+      $scope.cityIn='';
+      $scope.powerIn='';
+    };
+}]); //end myApp controller
--- routes/hero.js
+++ routes/hero.js
@@ -1,14 +1,29 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
+var Hero = require('../models/heroModel.js');
 
 // get all heros
-router.get('/', function(req, res) {
-
-});
+router.get('/', function(req, res){
+  console.log('GET route hit');
+  Hero.find({}, function(err, result){
+    if(err){
+      console.log('GET Broke');
+      res.sendStatus(500);
+    } else {
+      console.log('GET Sent');
+      res.send(result);
+    }
+  });
+});//end get
 
 // post to create a new hero
 router.post('/', function(req, res) {
-
+  console.log('Post route hit: ', req.body);
+  var postHero = Hero(req.body);
+  postHero.save();
+  console.log('PostHero ', postHero);
+  res.sendStatus( 200 );
 });
 
 module.exports = router;
